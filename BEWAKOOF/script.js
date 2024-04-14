642007507b1491421b7943a7bf5971a16ef81c59
Deepali Kasaudhan <kasaudhandeepali7@gmail.com>
2024-04-14 20:42:17 +0530

    Project

---
+++ BEWAKOOF/script.js
@@ -0,0 +1,19 @@
+$('.main-carousel').flickity({
+    // options
+    
+
+    groupCells: 2,
+    autoPlay:'true',
+    cellAlign: 'left',
+    wrapAround:true,
+    freeScrool: true
+  });
+
+  $('.main-carousel1').flickity({
+    // options
+    autoPlay:'true',
+    cellAlign: 'left',
+    wrapAround:true,
+    freeScrool: true,
+    groupCells: true
+  });
